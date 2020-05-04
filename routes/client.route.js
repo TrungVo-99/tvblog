e7347194d9409707e88ac368b331e2a2e7967315
ginganx201 <kientrung19120000@gmail.com>
2020-05-05 02:05:10 +0700

    make it better

--- routes/client.route.js
+++ routes/client.route.js
@@ -2,8 +2,8 @@ const { Router } = require("express");
 
 const router = Router();
 const clientController = require("../controllers/client.controller");
-router.get("/get-navbar", clientController.getMenuData);
 router.get("/", clientController.index);
+router.get("/get-navbar", clientController.getMenuData);
 router.get("/view-post/:postId", clientController.viewPost);
 
 module.exports = router;
